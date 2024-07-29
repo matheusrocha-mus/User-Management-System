c4317fe19ebac7f385eb0835cc4e4c032fba7cd0
Matheus Rocha <matheuscaetanorocha@gmail.com>
2024-07-29 05:55:40 -0300

    Update crete_user_table migration

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -17,9 +17,10 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->enum('role', ['USER', 'ADMIN']);
+            $table->integer('failed_login_attempts');
+            $table->boolean('account_locked');
             $table->rememberToken();
-            $table->foreignId('current_team_id')->nullable();
-            $table->string('profile_photo_path', 2048)->nullable();
             $table->timestamps();
         });
 
