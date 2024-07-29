952e6bfc1bbfe971969a5e4b36f300bb63aefb61
Matheus Rocha <matheuscaetanorocha@gmail.com>
2024-07-29 06:00:03 -0300

    Update FortiFyServiceProvider to handle user roles redirecting

--- app/Providers/FortifyServiceProvider.php
+++ app/Providers/FortifyServiceProvider.php
@@ -7,20 +7,41 @@ use App\Actions\Fortify\ResetUserPassword;
 use App\Actions\Fortify\UpdateUserPassword;
 use App\Actions\Fortify\UpdateUserProfileInformation;
 use Illuminate\Cache\RateLimiting\Limit;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\RateLimiter;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Str;
 use Laravel\Fortify\Fortify;
+use Laravel\Fortify\Contracts\LoginResponse;
+use Laravel\Fortify\Contracts\LogoutResponse;
 
 class FortifyServiceProvider extends ServiceProvider
 {
     /**
      * Register any application services.
      */
-    public function register(): void
+    public function register()
     {
-        //
+        $this->app->instance(LoginResponse::class, new class implements LoginResponse {
+            public function toResponse($request)
+            {
+                if($request->user()->role === 'ADMIN') {
+                    return redirect()->route('dashboard');
+                }
+                else if($request->user()->role === 'USER') {
+                    return redirect()->route('profile.show');
+                }
+            }
+        });
+
+        $this->app->instance(LogoutResponse::class, new class implements LogoutResponse {
+            public function toResponse($request)
+            {
+                return redirect('/');
+            }
+        });
     }
 
     /**
@@ -41,6 +62,19 @@ class FortifyServiceProvider extends ServiceProvider
 
         RateLimiter::for('two-factor', function (Request $request) {
             return Limit::perMinute(5)->by($request->session()->get('login.id'));
+        });
+
+        Fortify::authenticateUsing(function (Request $request) {
+            $user = User::where('email', $request->email)->first();
+
+            /* error_log('requestRole: '.$request->role);
+            error_log('userRole: '.$user->role); */
+
+            if ($user &&
+                Hash::check($request->password, $user->password) &&
+                $request->role === $user->role) {
+                return $user;
+            }
         });
     }
 }
