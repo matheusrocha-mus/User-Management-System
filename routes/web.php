2bea4a4ae7bc195d0e897d6aef1c32184052672e
Matheus Rocha <matheuscaetanorocha@gmail.com>
2024-07-29 06:02:18 -0300

    Update web routes

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -11,7 +12,9 @@ Route::middleware([
     config('jetstream.auth_session'),
     'verified',
 ])->group(function () {
-    Route::get('/dashboard', function () {
-        return view('dashboard');
-    })->name('dashboard');
+    Route::get('/dashboard', [UserController::class, 'dashboard'])->name('dashboard');
 });
+
+Route::get('/search', [UserController::class, 'search']);
+
+Route::delete('/user', [UserController::class, 'delete']);
